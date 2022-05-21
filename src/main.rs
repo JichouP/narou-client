73a7d2fdbf4a28f13a117d69e7d2c2cb5d67904b
JichouP <akkunn100@gmail.com>
2022-05-21 18:34:54 +0900

    :sparkles: hello world
    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <div>
+            <h1>{"Hello, world!"}</h1>
+        </div>
+    }
 }
